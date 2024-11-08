594d9d717a6d871672b60a6a9b291081033dcde0
devanshgoel15 <139127165+devanshgoel15@users.noreply.github.com>
2024-11-08 14:38:10 +0530

    Add files via upload

---
+++ TEST_CSDS_C/test.js
@@ -0,0 +1,15 @@
+const pricePerItem = 79900.00;
+
+document.getElementById('quantity').addEventListener('input', updateTotalPrice);
+
+function updateTotalPrice() {
+    const quantity = parseInt(document.getElementById('quantity').value);
+    const totalPrice = quantity * pricePerItem;
+    document.querySelector('.total-price').textContent = `Total Price: ₹${totalPrice.toFixed(2)}`;
+}
+
+function addToCart() {
+    const quantity = parseInt(document.getElementById('quantity').value);
+    const totalPrice = quantity * pricePerItem;
+    alert(`You added ${quantity} item(s) to your cart for ₹${totalPrice.toFixed(2)}.`);
+}
